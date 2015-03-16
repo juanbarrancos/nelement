49d11886196e2baf7da69caf1b0a87cfd6f66353
jbarrancos <jbarrancos@enchufados.com>
2015-03-16 17:15:59 +0100

    Version 0.6

--- js/nelement.js
+++ js/nelement.js
@@ -1,8 +1,13 @@
 
 $(document).ready(function() {
 	$(".nelement").focus(function(){
-		var next = "#" + $(this).data("next");
+		var next = $(this).data("next");
 		var length = $(this).data("length");
+		if(!next){
+			next = $(this).next('input');
+		}else{
+			next="#"+$(this).data("next");
+		}
 		if(!length){
 			length = $(this).attr('maxLength');
 		}
